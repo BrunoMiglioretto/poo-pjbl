574117bd88fa122a529781df7d402cecf786509f
Bruno Miglioretto <bruno.miglioretto@gmail.com>
2023-06-12 13:44:16 -0300

    remover classe programa

--- main.java
+++ main.java
@@ -1,7 +1,15 @@
+import app.contollers.ProdutoController;
 import app.views.JanelaPrincipal;
 
 public class main {
+    public static ProdutoController produtoController;    
+
     public static void main(String[] args) {
-        programa p = new programa();
+        produtoController = new ProdutoController();        
+
+        JanelaPrincipal janela = new JanelaPrincipal(produtoController);
+        janela.setDefaultCloseOperation(JanelaPrincipal.EXIT_ON_CLOSE);
+        janela.setSize(1024, 768);
+        janela.setVisible(true);
     }
 }
